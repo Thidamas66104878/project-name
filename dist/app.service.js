19eebdd5ea48eae2969a1a0c50fbc584fb570f43
Your Name <your.email@example.com>
2025-05-31 14:21:43 +0700

    it2

--- dist/app.service.js
+++ dist/app.service.js
@@ -10,7 +10,10 @@ exports.AppService = void 0;
 const common_1 = require("@nestjs/common");
 let AppService = class AppService {
     getHello() {
-        return 'Hello World!';
+        return {
+            name: 'Thidamas Srichai',
+            id: 66104878
+        };
     }
 };
 exports.AppService = AppService;
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/it')
+  getit(): string{
+    return 'it works';
+  }
 }
